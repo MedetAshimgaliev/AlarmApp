1f847518f9632e00d922e8d60ea45eef9a7b4a2e
Almaz Suraganov <almazsuraganov@Almazs-MacBook-Pro.local>
2017-11-23 23:57:38 +0600

    Initial Commit

---
+++ Medet's AlarmTests/Medet_s_AlarmTests.swift
@@ -0,0 +1,36 @@
+//
+//  Medet_s_AlarmTests.swift
+//  Medet's AlarmTests
+//
+//  Created by Almaz Suraganov on 23.11.17.
+//  Copyright © 2017 Almaz Suraganov. All rights reserved.
+//
+
+import XCTest
+@testable import Medet_s_Alarm
+
+class Medet_s_AlarmTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
